7b9539de64bca9a263e6d588f19f19b61af3b4d1
clara <89088484+ClaraCosta@users.noreply.github.com>
2024-08-12 22:14:33 -0300

    Utilizando novo alias no efore

--- cypress/e2e/booking.cy.js
+++ cypress/e2e/booking.cy.js
@@ -1,68 +1,123 @@
 describe('Test Suit - Auth API Booker', () => {
-    it('1 - Get all booking ids', () => {
-      cy.request({
-        method: 'GET',
-        url: '/booking',
-        headres: { 'Content-Type': 'application/json'}
-      }).then((response)=>{
-        expect(response.status).to.eq(200);
-        expect(response.body).to.be.an('array');
-        expect(response.body).to.have.lengthOf.at.least(1);
-        expect(response.body[0]).to.have.property('bookingid');
-  
-    });
 
-    
-})
+    let token = null;
 
-    it('2 - Get booking created by it "3 - Creating a new booking with success" ', () => {
+    before(() => {
+        //Gerando um token antes de cada teste
         cy.request({
-        method: 'GET',
-        url: '/booking/74',
-        headres: { 'Content-Type': 'application/json'}
-        }).then((response)=>{
-        expect(response.status).to.eq(200);
-        expect(response.body).to.have.a.property( "firstname");
-        expect(response.body).to.have.a.property( "depositpaid", true);
-
-    });
-
+            method: 'POST',
+            url: '/auth',
+            headres: { 'Content-Type': 'application/json'},
+            body: {
+                username: "admin",
+                password: "password123"
+            }
+        })
+        .then((response)=>{
+            token = response.body.token;
+            cy.log(JSON.stringify(token));
+            Cypress.env("token", token);
+        });
     })
 
-    it('3 - Creating a new booking with success', () => {
-        cy.request({
-        method: 'POST',
-        url: '/booking',
-        headres: { 'Content-Type': 'application/json'},
-        body:{
-            firstname: "Clara",
-            lastname: "Costa",
-            totalprice: 100,
-            depositpaid: true,
-            bookingdates: {
-                checkin: "2018-01-01",
-                checkout: "2019-01-01"
-            },
-            additionalneeds: 'Breakfast'
-        },
-        }).then((response)=>{
-        expect(response.status).to.eq(200);
-        expect(response.body).to.be.an( "object");
-        expect(response.body).to.have.a.property( "bookingid").and.to.be.a("number");
-        //Recuperando um teste através do alias
-        cy.wrap(response).as('bookingCreated', {type: 'static'});
 
-    }).then(function () {
+
+        it('1 - Get all booking ids', () => {
         cy.request({
             method: 'GET',
-            //Chamando id recuperado através do alias criado
-            url: '/booking/'+ this.bookingCreated.body.bookingid ,
-            headres: { 'Content-Type': 'application/json'},
-        }).then((response) =>{
+            url: '/booking',
+            headres: { 'Content-Type': 'application/json'}
+        }).then((response)=>{
             expect(response.status).to.eq(200);
+            expect(response.body).to.be.an('array');
+            expect(response.body).to.have.lengthOf.at.least(1);
+            expect(response.body[0]).to.have.property('bookingid');
+    
+        });
+
+        
+        })
+
+        it('2 - Get booking created by it "3 - Creating a new booking with success" ', () => {
+            cy.request({
+            method: 'GET',
+            url: '/booking/74',
+            headres: { 'Content-Type': 'application/json'}
+            }).then((response)=>{
+            expect(response.status).to.eq(200);
+            expect(response.body).to.have.a.property( "firstname");
+            expect(response.body).to.have.a.property( "depositpaid", true);
+
+        });
+
         })
         
-    });
-})
+
+        it('3 - Creating a new booking with success', () => {
+            cy.request({
+            method: 'POST',
+            url: '/booking',
+            headres: { 'Content-Type': 'application/json'},
+            body:{
+                firstname: "Clara",
+                lastname: "Costa",
+                totalprice: 100,
+                depositpaid: true,
+                bookingdates: {
+                    checkin: "2018-01-01",
+                    checkout: "2019-01-01"
+                },
+                additionalneeds: 'Breakfast'
+            },
+            }).then((response)=>{
+            expect(response.status).to.eq(200);
+            expect(response.body).to.be.an( "object");
+            expect(response.body).to.have.a.property( "bookingid").and.to.be.a("number");
+            //Recuperando um teste através do alias
+            cy.wrap(response).as('bookingCreated', {type: 'static'});
+
+        }).then(function () {
+            cy.request({
+                method: 'GET',
+                //Chamando id recuperado através do alias criado
+                url: '/booking/'+ this.bookingCreated.body.bookingid ,
+                headres: { 'Content-Type': 'application/json'},
+            }).then((response) =>{
+                expect(response.status).to.eq(200);
+            })
+            
+        });
+       
+
+        })
+
+        it('4 - Updating a booking with success', () => {
+            
+            cy.request({
+                method: 'PUT',
+                url: '/booking/30',
+                headers: {
+                    Accept: "application/json",
+                    Cookie: `token=${token}`,
+                  },
+                body:{
+                    firstname: "Another",
+                    lastname: "User",
+                    totalprice: 10,
+                    depositpaid: false,
+                    bookingdates: {
+                        checkin: "2018-01-01",
+                        checkout: "2019-01-01"
+                    },
+                    additionalneeds: 'Dinners'
+                },
+
+        }).then((response) =>{
+            cy.log(JSON.stringify(response.body));
+        })
+
+        
+                
+            })
 
 })
